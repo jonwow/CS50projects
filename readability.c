238414bfb8a4673bd451e1582a5d94d19919b93a
jonas <jonwow@protonmail.com>
2020-01-11 23:37:00 +0200

    Comments for readability & an input suggestion

--- readability.c
+++ readability.c
@@ -4,16 +4,22 @@
 #include <ctype.h>
 #include <math.h>
 
+// example input:
+/* Harry Potter was a highly unusual boy in many ways. For one thing, he hated the summer holidays more than any other time of year.
+ For another, he really wanted to do his homework, but was forced to do it in secret, in the dead of the night.
+ And he also happened to be a wizard. */
+
+
 int main(void)
 {
-    // L - avg number of letters per 100 words
-    // S - avg number of sentences per 100 words
+    // L - average number of letters per 100 words
+    // S - average number of sentences per 100 words
     // word = chars until space, sentences - words until .!?
-    // max 16, min 1
+    // min 1 grade, max 16 grade.
     int L = 0, S = 0, words = 1, index;
 
 
-    // ask the user for a text
+    // ask the user for some text
     string text = get_string("Text: ");
 
 
@@ -55,7 +61,7 @@ int main(void)
         multiplyBy = (float) words / 100;
     }
 
-
+    // math formula. index = the grade that the person should be in.
     index = round(0.0588 * (L * multiplyBy) - 0.296 * (S * multiplyBy) - 15.8);
 
     if (index > 16)
